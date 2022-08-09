16451436b6b44704665c1dce75d1f7f1ac99b597
Argo Roots <argo@roots.ee>
2022-08-09 17:54:44 +0300

    Use Entu SSG for build

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-entu.ee page
+# [www.entu.ee](https://www.entu.ee)
+
+It's built with [Entu SSG](https://entu.site)
--- entu_web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p /data/entu_web/code
-cd /data/entu_web/code
-
-git clone https://github.com/argoroots/entu-web.git ./
-git checkout master
-git pull
